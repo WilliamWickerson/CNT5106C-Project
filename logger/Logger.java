286109aa9ce7714399b21b6f0f0642c7b539944b
WilliamWickerson <willie7296@gmail.com>
2018-03-13 23:42:00 -0400

    Implemented write() and added getTime()

--- logger/Logger.java
+++ logger/Logger.java
@@ -1,19 +1,37 @@
 package logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Logger {
 
-	private String fileName;
+	private File logFile;
 	private int myId;
 	
 	public Logger(int id) {
 		myId = id;
-		fileName = "log_peer_" + myId + ".log";
+		logFile = new File("log_peer_" + myId + ".log");
+		logFile.delete();
+	}
+	
+	private String getTime() {
+		return new SimpleDateFormat("h:mm:ss a").format(new Date());
 	}
 	
 	private void write(String message) {
-		//TODO: write message to file
+		try {
+			BufferedWriter output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logFile, true)));
+			output.write(getTime() + ": " + message + System.lineSeparator());
+			output.close();
+		} catch(Exception e) {
+			System.out.println("Error: could not write to log file");
+			System.out.println(e.getClass().getName());
+		}
 	}
 	
 	public void connectedTo(int id) {
